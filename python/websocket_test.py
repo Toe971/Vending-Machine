49ae6adb70cdc853bcbc523c24d7e82a3bf3565e
toe971 <liew179@gmail.com>
2021-01-30 21:17:48 +0800

    added test file for websocket

--- python/websocket_test.py
+++ python/websocket_test.py
@@ -14,18 +14,21 @@ async def handler(websocket, path):
         await websocket.send(json.dumps(data))
         await asyncio.sleep(1)
 
-
-""" loop = asyncio.get_event_loop()
+start_server = websockets.serve(handler, "127.0.0.1", 5555)
+loop = asyncio.get_event_loop().run_until_complete(start_server)
 loop.set_debug(True)  # debug mode
-try:
+loop.close()
+
+
+""" try:
     loop.run_until_complete(handler("127.0.0.1", 5555))
 finally:
     loop.run_until_complete(loop.shutdown_asyncgens())
-    loop.close() """
+    loop.close()
 
 start_server = websockets.serve(handler, "127.0.0.1", 5555)
 
 
-asyncio.get_event_loop().run_until_complete()
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever() """
 
