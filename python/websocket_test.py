b137b745fe35d291fb6eea9899d07b2ed37ebb11
toe971 <liew179@gmail.com>
2021-01-30 20:43:36 +0800

    added test file for websocket

--- python/websocket_test.py
+++ python/websocket_test.py
@@ -4,17 +4,27 @@ import websockets
 import json
 import random
 async def handler(websocket, path):
-    while True:
-        data = [
-            {
-                "test": 1,
-                "test2": 2
-            }
-        ]
-        await websocket.send(json.dumps(data))
-        await asyncio.sleep(1)
-start_server = websockets.serve(handler, "127.0.0.1", 5555)
+    data = [
+        {
+            "test": 1,
+            "test2": 2
+        }
+    ]
+    await websocket.send(json.dumps(data))
+    await asyncio.sleep(1)
 
-asyncio.get_event_loop().run_until_complete(start_server)
+
+loop = asyncio.get_event_loop()
+loop.set_debug(True)  # debug mode
+try:
+    loop.run_until_complete(handler())
+finally:
+    loop.run_until_complete(loop.shutdown_asyncgens())
+    loop.close()
+
+""" start_server = websockets.serve(handler, "127.0.0.1", 5555) """
+
+
+""" asyncio.get_event_loop().run_until_complete()
 asyncio.get_event_loop().run_forever()
-print("value?")
+ """
